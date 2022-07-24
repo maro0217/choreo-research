1b52e9782a8e76ed136c64750f961644cae444ce
Maro Yoshi <ydaye07@gmail.com>
2022-07-24 16:34:18 +0900

    ✨ カテゴリごとの検索機能実装中...

---
+++ pages/api/category.js
@@ -0,0 +1,14 @@
+import { client } from "../../libs/client";
+
+const handler = async (req, res) => {
+    console.log(req.body);
+    
+    const data = await client.get({
+        endpoint: 'blog',
+        queries: { filters: `category[equals]${req.body.id}`}
+      })
+    
+    res.status(200).json(data)
+}
+
+export default handler
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,61 +1,123 @@
 import { useForm } from '@mantine/form';
-import { Box, TextInput, Button, Group } from '@mantine/core';
+import {TextInput, Header, createStyles, Select } from '@mantine/core';
 import { client } from '../libs/client';
 import Link from "next/link";
 import { useState } from 'react';
 
+const useStyles = createStyles((theme) => ({
+  header: {
+    paddingLeft: theme.spacing.md,
+    paddingRight: theme.spacing.md,
+  },
+
+  inner: {
+    height: 56,
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+
+  input: {
+    width: 100
+  }
+}));
+
 
 export const getStaticProps = async () => {
-  const data = await client.get({
+  const bodyData = await client.get({
     endpoint: 'blog',
+  })
+
+  const categoryData = await client.get({
+    endpoint: 'category',
   })
 
   return {
     props: {
-      blog: data.contents
+      bodies: bodyData.contents,
+      categories: categoryData.contents
     },
   };
 };
 
-  export default function Home({ blog }) {
-    const [search, setSearch] = useState({});
-    
+  export default function Home({ bodies, categories }) {
+    const { classes } = useStyles();
     const form = useForm({
-      initialValues: { name: ''}
+      initialValues: {
+         name: '', 
+         genre: ''
+        }
     });
+    const [search, setSearch] = useState();
+    const [select, setSelect] = useState();
 
-    const handleSubmit = async(e) => {
-      const q = e.name;
-      const data = await fetch("/api/search", {
-        method: "POST",
-        headers: {"Content-type": "application/json"},
-        body: JSON.stringify({ q }),
-      });
-      const json = await data.json();
-      
-      setSearch(json)
+    const handleSubmit = async (e) => {
+      console.log(JSON.stringify(e));
+        const q = e.name;
+        const data = await fetch("/api/search", {
+          method: "POST",
+          headers: {"Content-type": "application/json"},
+          body: JSON.stringify({ q }),
+        });
+        const json = await data.json();
+        setSearch(json.contents)    
     }
 
-    console.log(search);
+    const categorySubmit = async (e) => {
+        const id = categories.map((data) => {
+          if (data.name === e.genre) {
+            return data.id
+          }
+        })
+        const data = await fetch("/api/category", {
+          method: "POST",
+          headers: {"Content-type": "application/json"},
+          body: JSON.stringify({ id }),
+        });
+        const json = await data.json();
+        setSelect(json.contents)    
+    }
 
-    const contents = search ? search : blog;
+    const contents = () => {
+      if (search) {
+        return search;
+      } else if (select) {
+        return select;
+      } else {
+        return bodies;
+      }
+    }
+    const genre = categories.map((category) => category.name)
 
   return (
     <div>
-      <Box sx={{ maxWidth: 340 }} mx="auto">
+    <Header height={56} className={classes.header}>
+      <div className={classes.inner}>
         <form onSubmit={form.onSubmit(handleSubmit)}>
-          <TextInput label="Name" placeholder="Name" {...form.getInputProps('name')} />
-          <Group position="right" mt="md">
-            <Button type="submit">Submit</Button>
-          </Group>
+            <TextInput
+                  className={classes.input} 
+                  placeholder="どんなコレオをお探しですか？" 
+                  {...form.getInputProps('name')} 
+            />
         </form>
-      </Box>
+        <form onSubmit={form.onSubmit(categorySubmit) }>
+          <Select
+                data={genre}
+                searchable
+                placeholder="お探しのスタイルはどれですか？"
+                {...form.getInputProps('genre')}
+                onSearchChange={categorySubmit}
+          /> 
+        </form>
 
-      <ul>
-        {blog.map((blog) => (
+    </div>
+    </Header>
+
+      <ul style={{listStyle:"none"}}>
+        {contents().map((blog) => (
           <li key={blog.id}>
             <Link href={`/blog/${blog.id}`}>
-              <a>{blog.title}, {blog.genre}</a>
+              <a className='flex'>{blog.title}, {blog.category.name}</a>
             </Link>
           </li>
         ))}
