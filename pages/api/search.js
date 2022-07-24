afaf3cf3be41be9c18b0186372a2df77ffcdd5ed
Maro Yoshi <ydaye07@gmail.com>
2022-07-24 16:33:38 +0900

    🐛 本文検索がちゃんとできるようになった！

--- pages/api/search.js
+++ pages/api/search.js
@@ -1,13 +1,14 @@
 import { client } from "../../libs/client";
 
-const handler = async (req, res) => {
+const Search = async (req, res) => {
     console.log(req.body);
     
     const data = await client.get({
         endpoint: 'blog',
         queries: {q: `${req.body.q}`}
       })
-    console.log(data);
+    
+    res.status(200).json(data)
 }
 
-export default handler
+export default Search
