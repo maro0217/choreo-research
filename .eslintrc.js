a507873f00c6b0e7ceb9bbd1f907181326dd0c72
Maro Yoshi <ydaye07@gmail.com>
2022-07-26 11:53:18 +0900

    ✨ eslintとprittierの導入

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+    "extends": [
+        "next/core-web-vitals",
+        "prettier"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+
+    }
+}
