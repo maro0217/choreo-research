873b29e81a4204bf4da4e43d4dfafd6c36b209c2
Maro Yoshi <ydaye07@gmail.com>
2022-08-23 17:42:32 +0900

    ✨ ユーザー編集ページ

---
+++ src/pages/ProfileEdit/[id].tsx
@@ -0,0 +1,98 @@
+import { Button, Group, MultiSelect, Select, TextInput } from "@mantine/core";
+import { doc, setDoc } from "firebase/firestore";
+import { useRouter } from "next/router";
+import { FormEvent, useState } from "react";
+import { useAuthContext } from "src/AuthContext";
+import { db } from "src/firebase";
+
+
+const prefecture = ["北海道","青森県","岩手県","宮城県","秋田県","山形県","福島県",
+"茨城県","栃木県","群馬県","埼玉県","千葉県","東京都","神奈川県",
+"新潟県","富山県","石川県","福井県","山梨県","長野県","岐阜県",
+"静岡県","愛知県","三重県","滋賀県","京都府","大阪府","兵庫県",
+"奈良県","和歌山県","鳥取県","島根県","岡山県","広島県","山口県",
+"徳島県","香川県","愛媛県","高知県","福岡県","佐賀県","長崎県",
+"熊本県","大分県","宮崎県","鹿児島県","沖縄県"];
+
+const ProfileEdit = () => {
+    type String = string | null;
+    type Array = string[] | undefined;
+    const [name, setName] = useState<String>('');
+    const [type, setType] = useState<String>('');
+    const [style, setStyle] = useState<Array>([]);
+    const [like, setLike] = useState<Array>([]);
+    const [place, setPlace] = useState<String>('');
+    const { user } = useAuthContext();
+    const router = useRouter();
+    
+    const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        try {
+            console.log(e.currentTarget.value);
+            console.log(name);
+            if (type === 'Dancer') {
+                await setDoc(doc(db, "users", user?.uid), {
+                        name: name,
+                        type: type,
+                        style: style,
+                        place: place
+                    })
+            } else {
+                await setDoc(doc(db, "users", user?.uid), {
+                    name: name,
+                    type: type,
+                    like: like,
+                    place: place
+                })
+            }
+        } catch (error) {
+            console.log(error);
+        }
+        router.push(`/profile/${user?.uid}`)
+    }
+
+    return (
+        <div>
+            <Group  position="center" sx={{ padding: '2rem' }}>
+                <form onSubmit={handleSubmit}>
+                    <TextInput
+                        placeholder="Your name"
+                        label="name"
+                        onChange={(e) => setName(e.currentTarget.value)}
+                    />
+                    <Select
+                        data={['Dancer', 'Audience']}
+                        placeholder="Your type"
+                        label="type"
+                        onChange={(value) => setType(value)}
+                    />
+                    {type === 'Dancer'?
+                    <MultiSelect
+                        data={['HIPHOP', 'Urban Choreography', 'JAZZ', 'LOCK']}
+                        placeholder="Your Style"
+                        label="style"
+                        value={style}
+                        onChange={setStyle}
+                    /> : 
+                    <MultiSelect
+                        data={['HIPHOP', 'Urban Choreography', 'JAZZ', 'LOCK']}
+                        placeholder="Your Like"
+                        label="like"
+                        value={like}
+                        onChange={setLike}
+                    />
+                    }
+                    <Select
+                        data={prefecture}
+                        placeholder="Your Place"
+                        label="place"
+                        onChange={(value) => setPlace(value)}
+                    />
+                    <Button type="submit">Submit</Button>
+                </form>
+            </Group>
+        </div>
+    )
+}
+
+export default ProfileEdit
